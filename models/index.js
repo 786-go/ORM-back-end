2c1e99d9cbbbe483b4418e70420adad74e71852c
786-go <noorullahw@gmail.com>
2022-04-09 13:14:33 -0700

    initialized index file

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+})
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: "category_id"
+})
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongsToMany(ProductTag, {
+  foreignKey: "product_id"
+})
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(ProductTag, {
+  foreignKey: "tag_id"
+})
+
 
 module.exports = {
   Product,
